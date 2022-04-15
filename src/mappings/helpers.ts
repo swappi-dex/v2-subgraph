59a96e4464fe9b3c1cae899e26ed4eceba487c8b
Liber <junlei.gong@confluxnetwork.org>
2022-04-16 02:47:22 +0800

    valid code verified on testnet

--- src/mappings/helpers.ts
+++ src/mappings/helpers.ts
@@ -114,32 +114,13 @@ export function isNullEthValue(value: string): boolean {
 }
 
 export function fetchTokenSymbol(tokenAddress: Address): string {
-  // log.debug('TESTING: tokenAddress = {}', [tokenAddress.toHexString()])
-
   // static definitions overrides
   let staticDefinition = TokenDefinition.fromAddress(tokenAddress)
   if(staticDefinition != null) {
     return (staticDefinition as TokenDefinition).symbol
   }
 
-  let contract = ERC20.bind(tokenAddress)
-  let contractSymbolBytes = ERC20SymbolBytes.bind(tokenAddress)
-
-  // try types string and bytes32 for symbol
   let symbolValue = 'unknown'
-  let symbolResult = contract.try_symbol()
-  if (symbolResult.reverted) {
-    let symbolResultBytes = contractSymbolBytes.try_symbol()
-    if (!symbolResultBytes.reverted) {
-      // for broken pairs that have no symbol function exposed
-      if (!isNullEthValue(symbolResultBytes.value.toHexString())) {
-        symbolValue = symbolResultBytes.value.toString()
-      }
-    }
-  } else {
-    symbolValue = symbolResult.value
-  }
-
   return symbolValue
 }
 
@@ -150,47 +131,12 @@ export function fetchTokenName(tokenAddress: Address): string {
     return (staticDefinition as TokenDefinition).name
   }
 
-  let contract = ERC20.bind(tokenAddress)
-  let contractNameBytes = ERC20NameBytes.bind(tokenAddress)
-
-  // try types string and bytes32 for name
   let nameValue = 'unknown'
-  let nameResult = contract.try_name()
-  if (nameResult.reverted) {
-    let nameResultBytes = contractNameBytes.try_name()
-    if (!nameResultBytes.reverted) {
-      // for broken exchanges that have no name function exposed
-      if (!isNullEthValue(nameResultBytes.value.toHexString())) {
-        nameValue = nameResultBytes.value.toString()
-      }
-    }
-  } else {
-    nameValue = nameResult.value
-  }
-
   return nameValue
 }
 
 export function fetchTokenTotalSupply(tokenAddress: Address): BigInt {
-  // let token = Token.load(tokenAddress.toHexString())
-
-  // log.debug(' ----------------------------------------------------------------------------------- sss', [])
-  // if (token === null) {
-  //   return ZERO_BI
-  // } else {
-  //   log.debug(' ----------------------------------------------------------------------------------- bbb', [])
-  //   // log.debug(' ----------------------------------------------------------------------------------- ttt = {}', [token.totalSupply.toString()])
-  //   return token.totalSupply
-  // }
-
-  let contract = ERC20.bind(tokenAddress)
-  let totalSupplyValue = null
-  let totalSupplyResult = contract.try_totalSupply()
-  if (!totalSupplyResult.reverted) {
-    totalSupplyValue = totalSupplyResult as i32
-  }
-  return BigInt.fromI32(totalSupplyValue as i32)
-
+  return ZERO_BI
 }
 
 export function fetchTokenDecimals(tokenAddress: Address): BigInt {
@@ -200,13 +146,7 @@ export function fetchTokenDecimals(tokenAddress: Address): BigInt {
     return (staticDefinition as TokenDefinition).decimals
   }
 
-  let contract = ERC20.bind(tokenAddress)
-  // try types uint8 for decimals
   let decimalValue = null
-  let decimalResult = contract.try_decimals()
-  if (!decimalResult.reverted) {
-    decimalValue = decimalResult.value
-  }
   return BigInt.fromI32(decimalValue as i32)
 }
 
